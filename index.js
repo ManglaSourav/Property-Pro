8daea578e3515fafeda2a62594315dea28188960
Sourav Mangla <mangla.sourav96@gmail.com>
2019-07-19 01:32:56 +0530

    .

--- index.js
+++ index.js
@@ -24,6 +24,13 @@ app.use("/api/property", property);
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "client", "build", "index.html"));
 });
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "client/build")));
+  //
+  app.get("*", (req, res) => {
+    res.sendfile(path.join((__dirname = "client/build/index.html")));
+  });
+}
 
 const port = process.env.PORT || 5000;
 app.listen(port, function() {
