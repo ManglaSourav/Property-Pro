07a701373f6b1135f6e4f8a09f8d34b97b0e40fe
Sourav Mangla <mangla.sourav96@gmail.com>
2019-07-19 10:12:00 +0530

    .

--- index.js
+++ index.js
@@ -20,10 +20,15 @@ mongoose
 app.use("/api/users", users);
 app.use("/api/property", property);
 
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "client", "build", "index.html"));
-});
+// Server static assets if in production
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  app.use(express.static("client/build"));
 
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 const port = process.env.PORT || 5000;
 app.listen(port, function() {
